12e7d2100de3f67e38d15e805bef36f6b5fc607c
Anthony Bartholomew <abar15.abb@gmail.com>
2022-07-02 03:11:00 -0400

    updated shader to discard alpha pixels

--- assets/shaders/gameShader.fs
+++ assets/shaders/gameShader.fs
@@ -7,7 +7,7 @@ in vec2 TexCoord;
 uniform sampler2D mapTexture;
 
 void main(void){
-
-FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-FragColor = texture(mapTexture, TexCoord);
+vec4 texColor = texture(mapTexture, TexCoord);
+if (texColor.a < 0.1) discard;
+FragColor = texColor;
 } 
--- src/game.h
+++ src/game.h
@@ -153,11 +153,11 @@ private:
 
 		// load and generate the texture
 
-		unsigned char* data = stbi_load("src/sprites/tempMap.png", &width, &height, &nrChannels, 0);
+		unsigned char* data = stbi_load("assets/sprite_sheets/level_one.png", &width, &height, &nrChannels, 0);
 
 
 		if (data) {
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 			glGenerateMipmap(GL_TEXTURE_2D);
 		}
 		else {
