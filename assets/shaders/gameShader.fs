bff90486fa68629ace9e41059889cbf66a280492
Anthony Bartholomew <abar15.abb@gmail.com>
2022-07-02 02:54:22 -0400

    added shader folder

---
+++ assets/shaders/gameShader.fs
@@ -0,0 +1,13 @@
+#version 460 core
+
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+uniform sampler2D mapTexture;
+
+void main(void){
+
+FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+FragColor = texture(mapTexture, TexCoord);
+} 
